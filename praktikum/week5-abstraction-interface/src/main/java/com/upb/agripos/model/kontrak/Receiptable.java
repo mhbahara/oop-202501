4dda7711b33db015085e615c0e837e92637880dc
bngmra <bulanbintang1072@gmail.com>
2025-11-04 21:33:47 +0700

    week5-abstraction-interface

---
+++ praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/model/kontrak/Receiptable.java
@@ -0,0 +1,5 @@
+package model.kontrak;
+
+public interface Receiptable {
+    String cetakStruk();
+}
---
+++ praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/model/kontrak/Validatable.java
@@ -0,0 +1,5 @@
+package model.kontrak;
+
+public interface Validatable {
+    boolean validasi(); // misal validasi OTP/ PIN
+}
