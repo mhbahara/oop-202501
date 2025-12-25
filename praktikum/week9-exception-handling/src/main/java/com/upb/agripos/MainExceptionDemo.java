c5b54a6188da822d72e02e4f67f6a213a7dd57fa
bngmra <bulanbintang1072@gmail.com>
2025-12-25 23:42:11 +0700

    Week9-exception

---
+++ praktikum/week9-exception-handling/src/main/java/com/upb/agripos/MainExceptionDemo.java
@@ -0,0 +1,32 @@
+package com.upb.agripos;
+
+public class MainExceptionDemo {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, I am [Bunga Maura Aulya]-[240320562] (Week9-exception)");
+
+        ShoppingCart cart = new ShoppingCart();
+        Product p1 = new Product("P01", "Pupuk Organik", 25000, 3);
+
+        try {
+            cart.addProduct(p1, -1);
+        } catch (InvalidQuantityException e) {
+            System.out.println("Kesalahan: " + e.getMessage());
+        }
+
+        try {
+            cart.removeProduct(p1);
+        } catch (ProductNotFoundException e) {
+            System.out.println("Kesalahan: " + e.getMessage());
+        }
+
+        try {
+            cart.addProduct(p1, 5);
+            cart.checkout();
+        } catch (Exception e) {
+            System.out.println("Kesalahan: " + e.getMessage());
+        }
+    }
+}
